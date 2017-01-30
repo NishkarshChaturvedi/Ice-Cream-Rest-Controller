f9f4202b87cd8f7deb824f8bbc281b0edc557eda
Nishtha Rathore <Nishtha Rathore@MyPersonal>
2017-01-30 09:57:47 +0530

    THird Commit

---
+++ src/main/resources/CartRestController.java
@@ -0,0 +1,33 @@
+package com.niit.myride.rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import backend.dao.CartDao;
+import backend.dao.ItemDao;
+import backend.model.Cart;
+import backend.model.Item;
+
+@RestController
+public class CartRestController {
+
+	@Autowired
+	CartDao cartDao;
+	
+	@Autowired
+	ItemDao itemDao;
+	
+	@RequestMapping(value = "/refresh/{cartId}")
+	public Cart getCartById(@PathVariable("cartId") int cartId) {
+		return cartDao.getById(cartId);
+	}
+	
+	@RequestMapping(value = "/remove/{itemId}", method = RequestMethod.PUT)
+	public void removeItem(@PathVariable("itemId") int itemId) {
+		Item item = itemDao.getItemById(itemId);
+		itemDao.remove(item);
+	}
+}
