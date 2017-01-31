28fb2ec0b85867fd131c623eedcc198965f09791
Nishtha Rathore <Nishtha Rathore@MyPersonal>
2017-01-31 18:08:01 +0530

    Some Change

--- src/main/resources/CartRestController.java
+++ src/main/resources/CartRestController.java
@@ -1,4 +1,4 @@
-package com.niit.myride.rest.controller;
+package restfrontend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
